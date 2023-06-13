f66a0602f56af7f24ecf8ed17f1481ed1142bbb7
RegularPhoenix <artiom.phoenix@gmail.com>
2023-06-13 22:16:51 +0300

    Add initial documentation

--- src/image/bitmap.h
+++ src/image/bitmap.h
@@ -3,70 +3,134 @@
 
 #include "image.h"
 
+/// @struct Bitmap
+/// @brief Class representing a bitmap picture (Microsoft BMP)
 struct Bitmap : Image {
    public:
+    /// Main constructor. Implicitly calls Bitmap::read(std::string)
     Bitmap(std::string file_name);
 
+    /// @brief Read data from .bmp file to this instance.
+    /// @param file_name Name of the file to read from.
+    /// @return The reference to this bitmap.
     Bitmap& read(std::string file_name);
 
+    /// @brief Write current data to the target file.
+    /// @param file_name Name of the file to write to.
+    /// @return The reference to this bitmap.
     Bitmap& write(std::string file_name);
 
+    /// @brief Replace current vector containing pixel data with a new one.
+    /// @param new_pixel_data New vector of bytes.
+    /// @return The reference to this bitmap.
     Bitmap& set_pixel_data(std::vector<uint8_t> new_pixel_data) override;
 
+    /// @brief Get current pixel data.
+    /// @return Vector of pixel values as bytes.
     std::vector<uint8_t>& get_pixel_data() override;
 
+    /// @brief Change bitmap dimensions.
+    /// @param width New width.
+    /// @param height New height.
+    /// @return The reference to this bitmap.
     Bitmap& set_dimensions(uint32_t width, uint32_t height) override;
 
+    /// @brief Get current bitmap width.
+    /// @return Width of the bitmap as unsigned int.
     uint32_t get_width() override;
 
+    /// @brief Get current bitmap height.
+    /// @return Height of the bitmap as unsigned int.
     uint32_t get_height() override;
 
+    /// @brief Get horizontal resolution current bitmap.
+    /// @return Horizontal resolution of the bitmap (in pixels per meter).
     uint32_t get_horizontal_resolution() override;
 
+    /// @brief Get vertical resolution current bitmap.
+    /// @return Vertical resolution of the bitmap (in pixels per meter).
     uint32_t get_vertical_resolution() override;
 
+    /// @brief Get number of channels of current bitmap.
+    /// @return Number of channels.
     uint8_t get_channels() override;
 
+    /// @brief Crop the bitmap.
+    /// @param x0 Vertical starting point.
+    /// @param y0 Horizontal starting point.
+    /// @param width Width of target bitmap.
+    /// @param height height of target bitmap.
+    /// @return The reference to this bitmap.
     Bitmap& crop(uint32_t x0, uint32_t y0, uint32_t width, uint32_t height) override;
 
+    /// @brief Flip the bitmap.
+    /// @return The reference to this bitmap.
     Bitmap& flip() override;
 
+    /// @brief Rotate the bitmap clockwise (90 degrees).
+    /// @return The reference to this bitmap.
     Bitmap& rotate_clockwise() override;
 
+    /// @brief Rotate the bitmap counterclockwise (270 degrees).
+    /// @return The reference to this bitmap.
     Bitmap& rotate_counterclockwise() override;
 
+    /// @brief Convert the bitmap to grayscale. Does not change the color depth.
+    /// @return The reference to this bitmap.
     Bitmap& grayscale() override;
 
+    /// @brief Invert the colors of this bitmap.
+    /// @return The reference to this bitmap.
     Bitmap& inverse() override;
 
+    /// @brief Resize the bitmap.
+    /// @param width Width of target bitmap.
+    /// @param height height of target bitmap.
+    /// @param mode Interpolation type.
+    /// @return The reference to this bitmap.
     Bitmap& resize(uint32_t width, uint32_t height, Interpolation mode) override;
 
+    /// @brief Change value of pixel at given coordinates.
+    /// @param x X coordinate of target pixel.
+    /// @param y Y coordinate of target pixel.
+    /// @param color New pixel value.
+    /// @return The reference to this bitmap.
     Bitmap& set_pixel(uint32_t x, uint32_t y, Color color) override;
 
+    /// @brief Get value of pixel at given coordinates.
+    /// @param x X coordinate of target pixel.
+    /// @param y Y coordinate of target pixel.
+    /// @return RGB value of pixel as Color.
     Color get_pixel(uint32_t x, uint32_t y) override;
 
    private:
+    /// @struct Bitmap header
+    /// @brief Stores general information about the bitmap image file.
     struct Header {
-        uint16_t signature{0x4D42};
-        uint32_t file_size{0};
-        uint32_t reserved{0};
-        uint32_t data_offset{0};
+        uint16_t signature{0x4D42}; /// Bitmap signature. All bmp file have these bytes at the beginning.
+        uint32_t file_size{0}; /// Size of file in bytes.
+        uint32_t reserved{0}; /// Reserved byte. Unused.
+        uint32_t data_offset{0}; /// File offset to raster data.
     } __attribute__((packed));
 
+    /// @struct Bitmap info header
+    /// @brief Stores detailed information about the bitmap image and defines the pixel format.
     struct InfoHeader {
-        uint32_t header_size{0};
-        int32_t image_width{0};
-        int32_t image_height{0};
-        uint16_t planes{0};
-        uint16_t bits_per_pixel{0};
-        uint32_t compression{0};
-        uint32_t image_size{0};
-        int32_t x_pixels_per_meter{0};  // INFO: Seems to be absolutely useless, but may be worth calculating later
-        int32_t y_pixels_per_meter{0};
-        uint32_t colors_used{0};
-        uint32_t colors_important{0};
+        uint32_t header_size{0}; /// Size of the info header in bytes. Must always be 40.
+        int32_t image_width{0}; /// Bitmap width.
+        int32_t image_height{0}; /// Bitmap height.
+        uint16_t planes{0}; /// Number of planes. Always 1.
+        uint16_t bits_per_pixel{0}; /// Image depth.
+        uint32_t compression{0}; /// Type of compression. Not used, always 0.
+        uint32_t image_size{0}; /// Size of compressed image. Since compression is always zero, this is 0 too.
+        int32_t x_pixels_per_meter{0}; /// Image horizontal resolution. Unused.
+        int32_t y_pixels_per_meter{0}; /// Image horizontal resolution. Unused.
+        uint32_t colors_used{0}; /// Number of actually used colors. Unused.
+        uint32_t colors_important{0}; /// Number of important colors. Unused.
     };
 
+    /// @struct Color data
+    /// @brief Used to define colors used by the bitmap image data. Mandatory for color depths <= 8 bits.
     struct ColorData {
         uint8_t red_intensity{0};
         uint8_t green_intensity{0};
@@ -74,10 +138,10 @@ struct Bitmap : Image {
         uint8_t reserved{0};
     };
 
-    Header header;
-    InfoHeader info_header;
-    std::vector<ColorData> color_table;
-    std::vector<uint8_t> pixel_data;
+    Header header; /// Current bitmap header
+    InfoHeader info_header; /// Current bitmap info header
+    std::vector<ColorData> color_table; /// Current bitmap color table
+    std::vector<uint8_t> pixel_data; /// Current bitmap pixel data (as bytes)
 };
 
 #endif
--- src/image/gif.h
+++ src/image/gif.h
@@ -3,92 +3,159 @@
 
 #include "image.h"
 
+/// @struct GIF
+/// @brief Class representing a gif picture (Graphics Interchange Format)
+/// Currently does not implement animation and transparency (supports only GIF87)
 struct GIF : Image {
    public:
+    /// Main constructor. Implicitly calls GIF::read(std::string)
     GIF(std::string file_name);
 
+    /// @brief Read data from .gif file to this instance.
+    /// @param file_name Name of the file to read from.
+    /// @return The reference to this image.
     GIF& read(std::string file_name);
 
+    /// @brief Write current data to the target file.
+    /// @param file_name Name of the file to write to.
+    /// @return The reference to this image.
     GIF& write(std::string file_name);
 
+    /// @brief Replace current vector containing pixel data with a new one.
+    /// @param new_pixel_data New vector of bytes.
+    /// @return The reference to this image.
     GIF& set_pixel_data(std::vector<uint8_t> new_pixel_data) override;
 
+    /// @brief Get current pixel data.
+    /// @return Vector of pixel values as bytes.
     std::vector<uint8_t>& get_pixel_data() override;
 
+    /// @brief Change image dimensions.
+    /// @param width New width.
+    /// @param height New height.
+    /// @return The reference to this image.
     GIF& set_dimensions(uint32_t width, uint32_t height) override;
 
+    /// @brief Get current image width.
+    /// @return Width of the image as unsigned int.
     uint32_t get_width() override;
 
+    /// @brief Get current image height.
+    /// @return Height of the image as unsigned int.
     uint32_t get_height() override;
 
+    /// @brief Get horizontal resolution current image.
+    /// @return Horizontal resolution of the image (in pixels per meter).
     uint32_t get_horizontal_resolution() override;
 
+    /// @brief Get vertical resolution current image.
+    /// @return Vertical resolution of the image (in pixels per meter).
     uint32_t get_vertical_resolution() override;
 
+    /// @brief Get number of channels of current image.
+    /// @return Number of channels.
     uint8_t get_channels() override;
 
+    /// @brief Crop the image.
+    /// @param x0 Vertical starting point.
+    /// @param y0 Horizontal starting point.
+    /// @param width Width of target image.
+    /// @param height height of target image.
+    /// @return The reference to this image.
     GIF& crop(uint32_t x0, uint32_t y0, uint32_t width, uint32_t height) override;
 
+    /// @brief Flip the image.
+    /// @return The reference to this image.
     GIF& flip() override;
 
+    /// @brief Rotate the image clockwise (90 degrees).
+    /// @return The reference to this image.
     GIF& rotate_clockwise() override;
 
+    /// @brief Rotate the image counterclockwise (270 degrees).
+    /// @return The reference to this image.
     GIF& rotate_counterclockwise() override;
 
+    /// @brief Convert the image to grayscale. Does not change the color depth.
+    /// @return The reference to this image.
     GIF& grayscale() override;
 
+    /// @brief Invert the colors of this image.
+    /// @return The reference to this image.
     GIF& inverse() override;
 
+    /// @brief Resize the image.
+    /// @param width Width of target image.
+    /// @param height height of target image.
+    /// @param mode Interpolation type.
+    /// @return The reference to this image.
     GIF& resize(uint32_t width, uint32_t height, Interpolation mode) override;
 
+    /// @brief Change value of pixel at given coordinates.
+    /// @param x X coordinate of target pixel.
+    /// @param y Y coordinate of target pixel.
+    /// @param color New pixel value.
+    /// @return The reference to this image.
     GIF& set_pixel(uint32_t x, uint32_t y, Color color) override;
 
+    /// @brief Get value of pixel at given coordinates.
+    /// @param x X coordinate of target pixel.
+    /// @param y Y coordinate of target pixel.
+    /// @return RGB value of pixel as Color.
     Color get_pixel(uint32_t x, uint32_t y) override;
 
    private:
-    // Exactly 6 bytes
+    /// @struct GIF header
+    /// @brief All GIF files must start with a header block.
     struct Header {
-        uint8_t signature[3]{0x47, 0x49, 0x46};
-        uint8_t version[3]{0x38, 0x39, 0x61};  // GIF89a is default
+        uint8_t signature[3]{0x47, 0x49, 0x46}; /// GIF signature.
+        uint8_t version[3]{0x38, 0x39, 0x61};  /// GIF version (87a or 89a).
     };
 
-    // Exactly 7 bytes
+    /// @struct Logical Screen Descriptor
+    /// @brief This block tells the decoder how much room this image will take up. It is exactly seven bytes long.
     struct LogicalScreenDescriptor {
-        uint8_t canvas_width[2]{0, 0};
-        uint8_t canvas_height[2]{0, 0};
-        uint8_t packed_field{0};
-        uint8_t background_color_index{0};
-        uint8_t pixel_aspect_ratio{0};
+        uint8_t canvas_width[2]{0, 0}; /// Used to specified canvas width. Not used nowadays.
+        uint8_t canvas_height[2]{0, 0};  /// Used to specified canvas height. Not used nowadays.
+        uint8_t packed_field{0}; /// Contains four fields of packed data.
+        uint8_t background_color_index{0}; /// The index of GCT color to use as background. Not used nowadays.
+        uint8_t pixel_aspect_ratio{0}; /// Pixel aspect ratio. Not used nowadays.
     } __attribute__((packed));
 
+    /// @struct Actual GIF image
+    /// @brief Contains data of the actual image.
     struct GifImage {
+        /// @struct Image descriptor
+        /// @brief Each image begins with an image descriptor block. This block is exactly 10 bytes long.
         struct ImageDescriptor {
-            uint8_t separator{0x2C};
-            uint16_t image_left{0};
-            uint16_t image_top{0};
-            uint16_t image_width{0};
-            uint16_t image_height{0};
-            uint8_t packed_field{0};
+            uint8_t separator{0x2C}; /// Every image descriptor begins with the value 2C.
+            uint16_t image_left{0}; /// Image left position. Not used nowadays.
+            uint16_t image_top{0}; /// Image right position. Not used nowadays.
+            uint16_t image_width{0}; /// How many space image takes on the canvas. Not used nowadays.
+            uint16_t image_height{0}; /// How many space image takes on the canvas. Not used nowadays.
+            uint8_t packed_field{0}; /// Contains five fields of packed data.
         } __attribute__((packed));
 
+        /// @struct Data sub-block
+        /// @brief The image data is composed of a series of output codes which tell the decoder which colors to emit to the canvas.
         struct DataSubBlock {  // Continiously read these until 0 length is met
-            uint8_t length;
-            std::vector<uint8_t> data;
+            uint8_t length; /// How many bytes of actual data follow.
+            std::vector<uint8_t> data; /// The data itself.
         };
 
         std::vector<uint8_t> full_pixel_data();
 
-        ImageDescriptor image_descriptor;
+        ImageDescriptor image_descriptor; /// Current image descriptor.
         // std::set<Color> local_color_table; TODO: Local color table support
-        uint8_t lzw_minimum_code_size;
-        std::vector<DataSubBlock> pixel_data;
+        uint8_t lzw_minimum_code_size; // Move to sub-block
+        std::vector<DataSubBlock> pixel_data; // Move sub-block from struct fo fields?
     };
 
-    Header header;
-    LogicalScreenDescriptor lsd;
-    std::set<Color> global_color_table;
-    std::vector<GifImage> images;
-    uint8_t trailer{0x3B};
+    Header header; /// Current image header.
+    LogicalScreenDescriptor lsd; /// Current image loginal screen descriptor.
+    std::set<Color> global_color_table; // Not needed to keep?
+    std::vector<GifImage> images; /// All the stored images.
+    uint8_t trailer{0x3B}; /// Trailer.
 };
 
 #endif
