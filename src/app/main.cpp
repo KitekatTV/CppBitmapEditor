58b21fb9ce9b787861e92ed5ff96e73eafee2e1e
RegularPhoenix <artiom.phoenix@gmail.com>
2023-06-14 23:13:01 +0300

    Add simple cli

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,78 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 
 #include "image.h"
+#include "bitmap.h"
+#include "gif.h"
 
-int main() {
-    bool is_windows{false};
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Help text" << std::endl;
+        return 1;
+    }
+
+    std::string command = argv[1];
+    bool no_input = argc < 3;
+
+    if (no_input) {
+        std::cout << "Command help" << std::endl;
+        return 1;
+    }
+
+    std::filesystem::path input_file = argv[2];
+
+    bool is_gif = false;
+
+    if (input_file.extension() == ".gif") {
+        is_gif = true;
+    } else if (input_file.extension() != ".bmp") {
+        std::cout << "Given file is not a GIF or Bitmap" << std::endl;
+        return 1;
+    }
+
+    if (!is_gif) {
+        Bitmap bitmap(input_file.string());
+        if (command == "crop") {
+            bitmap.crop(0, 0, 100, 100).write(input_file.string());
+        } else if (command == "flip") {
+            bitmap.flip().write(input_file.string());
+        } else if (command == "rcw") {
+            bitmap.rotate_clockwise().write(input_file.string());
+        } else if (command == "rccw") {
+            bitmap.rotate_counterclockwise().write(input_file.string());
+        } else if (command == "grayscale") {
+            bitmap.grayscale().write(input_file.string());
+        } else if (command == "inverse") {
+            bitmap.inverse().write(input_file.string());
+        } else if (command == "resize") {
+            bitmap.resize(560, 560, Interpolation::NearestNeighbour).write(input_file.string());
+        } else if (command == "test") {
+            std::cout << "Testing write\n" << std::endl;
+            bitmap.write(input_file.string());
+        }
+    }
+    /* if (is_gif) {
+        GIF gif{input_file.string()};
+        if (command == "crop") {
+            gif.crop(0, 0, 100, 100).write(input_file.string());
+        } else if (command == "flip") {
+            gif.flip().write(input_file.string());
+        } else if (command == "rcw") {
+            gif.rotate_clockwise().write(input_file.string());
+        } else if (command == "rccw") {
+            gif.rotate_counterclockwise().write(input_file.string());
+        } else if (command == "grayscale") {
+            gif.grayscale().write(input_file.string());
+        } else if (command == "inverse") {
+            gif.inverse().write(input_file.string());
+        } else if (command == "resize") {
+            gif.resize(560, 560, Interpolation::NearestNeighbour).write(input_file.string());
+        }
+    }*/
+
+    return 0;
+    /* bool is_windows{false};
 
 #if defined _WIN32 || defined _WIN64
     is_windows = true;
@@ -54,5 +122,9 @@ int main() {
         bmp.write(destination + "test_grayscale.bmp");
     }
 
-    return 0;
+    return 0;*/
+}
+
+void act(Image& img) {
+
 }
